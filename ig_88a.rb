760a1235e4a48fb40641917c1c82dae7349ef5a5
Joy <stepjoyous@gmail.com>
2019-01-03 13:48:17 -0800

    Completed 3rd exercise in Star Wars control flow

--- ig_88a.rb
+++ ig_88a.rb
@@ -1,6 +1,6 @@
-who_r_u = ""
+gunfight = 1
 
-while (who_r_u.downcase != "han solo")
+while gunfight ==1
     puts "Freeze! Who are you?"
     who_r_u = gets.chomp
     if who_r_u.downcase == "zap"
@@ -10,7 +10,17 @@ while (who_r_u.downcase != "han solo")
         puts "Phew! You've escaped!"
         break
     elsif who_r_u.downcase == "han solo"
-       puts "Hello, #{who_r_u.upcase}!"     
+        puts "You are mine at last, Solo!!!" 
+        for z in 1..20
+            puts "(zap!)"
+        end
+        caught_or_not = Random.new
+            if caught_or_not.rand(100) > 49
+                puts "Solo is caught! Jabba the Hutt will pay handsomely... MUAHAHAHAHA..."
+                break
+            else
+                puts "I'll get you next time, Solooooo!!!"
+            end  
     else
         puts "You must die!!! (zap zap kaboom)!"
     end
